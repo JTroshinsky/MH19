25391a0e10e4da82c3e3414dbd915ad2cf09403c
Dominic Voto <votox002@umn.edu>
2019-02-03 02:25:57 -0600

    Test commit 7

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
