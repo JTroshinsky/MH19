1f71bd23708180b406c8e79c383795595e751c5c
trosh003 <trosh003@umn.edu>
2019-02-03 04:08:22 -0600

    mappap

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 const key = 'pk.eyJ1IjoianRyb3NoaW5za3kiLCJhIjoiY2puaWI3a3JoMDc0ajNrcjE5Mmhhb3EyZCJ9.fZ8G7cdM8RjoPaiwq01aYQ';
 // Create an instance of Mapboxgl
-const mappa = new Mappa('Mapbox', key);
+const mappa = new Mappa('MapboxGL', key);
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -10,7 +10,8 @@ const options = {
   lat: 44.797520,
   lng: -92.936900,
   zoom: 15,
-  style: 'mapbox://styles/mapbox/satellite-streets-v9',
+  style: 'mapbox://styles/mapbox/traffic-night-v2',
+  pitch: 50,
 };
 
 const myMap = mappa.tileMap(options);
