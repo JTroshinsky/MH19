b415a06448dfdce1d47ffcfe3049c923e1bc07ca
Dominic Voto <votox002@umn.edu>
2019-02-03 02:20:29 -0600

    Test commit 5

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => res.render('index.html'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
