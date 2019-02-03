e8604b8947cf7dd7b6a1913c8f39dc011f9178c4
trosh003 <trosh003@umn.edu>
2019-02-03 07:56:22 -0600

    hjk

--- public/index.js
+++ public/index.js
@@ -78,7 +78,7 @@ function drawPoints(){
   for (var x = 0; x<matrix.length;x++){
     var row = matrix[x];
     for(var y= 0; y<row.length; y++){
-      console.log(row[y].)
+      console.log(row[y].colorValue)
       fill(150);
       rect(row[y].xPos,row[y].yPos,20,20);
     }
@@ -186,7 +186,7 @@ class tile{
   }
 
 
-  setColor(var col){
+  setColor(col){
     this.colorValue = col;
   }
 }
